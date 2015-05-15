f4ed81a99a284e9fea0668ed081a8c695f3f7f7d
Melvin Low <melvin.w.low@gmail.com>
2015-05-15 01:35:50 -0700

    major boilerplate stuff, some tested, some untested

--- src/Main.hs
+++ src/Main.hs
@@ -1,54 +1,105 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-import Control.Concurrent (threadDelay)
-import Control.Monad (forever)
+
+import System.Environment
+import System.Console.ANSI
+import System.Exit
+import System.Posix.Signals
+import Control.Concurrent 
+import Control.Monad 
+import Control.Applicative
 import Control.Distributed.Process
-import Control.Distributed.Process.Node
-import Network.Transport.TCP (createTransport, defaultTCPParameters)
-import Data.Binary
-import Data.Typeable
-import GHC.Generics (Generic)
+import Control.Distributed.Process.Node hiding (newLocalNode)
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Text.Read
+import Text.Printf
 
 import Raft.Protocol
 
-data SInt = SInt Int deriving (Generic, Typeable)
+-- | Colors!
+setColor :: Color -> IO ()
+setColor c = setSGR [SetColor Foreground Dull c]
 
-instance Binary SInt
+resetColor :: IO ()
+resetColor = setSGR [Reset]
 
-replyBack :: (ProcessId, RpcString) -> Process ()
-replyBack (sender, RpcString s) = send sender s
+color :: Color -> IO a -> IO a
+color c action = setColor c >> action <* resetColor
 
-logMessage :: String -> Process ()
-logMessage msg = say $ "handling " ++ msg
+-- | Cleanup on exit.
+cleanUp :: [LocalNode] -> IO ()
+cleanUp nodes = do
+    color Cyan $ putStrLn "==> Cleaning up"
+    mapM_ closeLocalNode nodes
+
+-- | Initialize the cluster.
+initCluster :: String -> String -> Int -> IO ()
+initCluster host port numServers = do
+    -- Initialize backend and then start all the nodes
+    color Cyan $ printf "%s %d %s\n" "==> Starting up" numServers "nodes"
+    backend <- initializeBackend host port initRemoteTable
+    nodes <- replicateM numServers $ newLocalNode backend
+    installHandler keyboardSignal (Catch (cleanUp nodes)) Nothing
+
+    -- Wait awhile to let nodes start
+    threadDelay 1000000
+
+    -- Find and count peers
+    count <- liftM length $ findPeers backend 1000000 >>= mapM print
+    putStr "Nodes detected: "
+    if count == numServers
+        then color Green $ printf "%d%s%d\n" count "/" numServers
+        else color Red $ printf "%d%s%d\n" count "/" numServers
+
+    -- Run Raft on all of the nodes
+    threadDelay 500000
+    color Cyan . putStrLn $ "==> Running Raft"
+    processes <- mapM (`forkProcess` initRaft backend nodes) nodes
+    
+    -- Run partition experiments...
+    threadDelay 500000
+    
+    -- Clean up
+    cleanUp nodes
+
 
 main :: IO ()
 main = do
-  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
-  node <- newLocalNode t initRemoteTable
+    -- Parse command line arguments
+    args <- getArgs
+    case args of
+        [host, port, numServers] ->
+            case readMaybe numServers of
+                Nothing -> putStrLn "usage: raft host port numServers"
+                -- Start the cluster
+                Just n -> initCluster host port n
+        _ -> putStrLn "usage: raft host port numServers"
+
+  {-
+  backend <- initializeBackend "127.0.0.1" "10501" initRemoteTable
+  node <- newLocalNode backend
+  node2 <- newLocalNode backend
+  liftIO $ threadDelay (1*1000000)
+  findPeers backend 1000000 >>= print
+
   forkProcess node $ do
-    -- Spawn another worker on the local node 
     echoPid <- spawnLocal $ forever $ do
-      -- Test our matches in order against each message in the queue
       receiveWait [match logMessage, match replyBack]
 
-    -- The `say` function sends a message to a process registered as "logger".
-    -- By default, this process simply loops through its mailbox and sends
-    -- any received log message strings it finds to stderr.
 
     say "send some messages!"
     send echoPid "hello"
     self <- getSelfPid
-    send echoPid (self, RpcString "hi")
+    send echoPid (self, [True::Bool])
 
-    -- `expectTimeout` waits for a message or times out after "delay"
     m <- expectTimeout 1000000
     case m of
-      -- Die immediately - throws a ProcessExitException with the given reason.
       Nothing  -> die "nothing came back!"
       (Just s) -> say $ "got " ++ s ++ " back!"
     return ()
 
-  -- A 1 second wait. Otherwise the main thread can terminate before
-  -- our messages reach the logging process or get flushed to stdio
   liftIO $ threadDelay (1*1000000)
   return ()
+  -}
+
+
+
+
