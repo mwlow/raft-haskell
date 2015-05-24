141aeaa674a285a8e5d1dc13e5acc36af054802b
AlaRuba <gabrieldkho@DN525eh8.SUNet>
2015-05-23 22:33:15 -0700

    Added changes to main

---
+++ src/Test.hs
@@ -0,0 +1,39 @@
+module Test where
+
+import System.Random.MWC
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Monad
+import Control.Monad.Trans
+import Control.Applicative
+import Control.Distributed.Process
+import Control.Distributed.Process.Extras (isProcessAlive)
+import Control.Distributed.Process.Node hiding (newLocalNode)
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Distributed.Process.Serializable
+import Control.Distributed.Process.Internal.Types
+import qualified Data.Map as Map
+
+import Raft.Protocol
+
+
+testCalls :: [LocalNode] -> [a0] -> Process()
+testCalls nodes processes = do  
+  -- Code to run tests. Zip node and processes into a map
+  -- then send to test calls
+  let
+    nodeIDs = map localNodeId nodes
+    nodeList = zip nodeIDs processes
+    m = Map.fromList (nodeList)
+  randomGen <- liftIO createSystemRandom
+
+  randomValue <- liftIO (uniformR (0, Map.size m - 1) (randomGen) :: IO Int)
+  let
+    randomNodeId = Map.keys m !! randomValue
+    randomProcessId = Map.lookup randomNodeId m
+
+
+
+  say "LOOK AT MEEEEEE" 
+  say (show randomNodeId)
+  return()
