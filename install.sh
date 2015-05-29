50b3dbf9a63d636ac76e4bcb55a8a79b8441beaa
Melvin Low <melvin.w.low@gmail.com>
2015-05-28 23:48:12 -0700

    modified install script

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+color=`tput setaf 4`
+reset=`tput sgr0`
+
+echo "${color}==> Creating sandbox${reset}"
+cabal sandbox init
+
+echo "${color}==> Fetching third party dependencies${reset}"
 git submodule init
 git submodule update
 
-cabal sandbox init
+echo "${color}==> Installing dependencies${reset}"
 cabal install sources/cloud-haskell sources/distributed-process-simplelocalnet/
 cabal install --only-dependencies
+
+echo "${color}==> Building${reset}"
 cabal build
